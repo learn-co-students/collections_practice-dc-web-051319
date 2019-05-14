9bc5c3d7014f8be530183bd144eec962b8ed7ea8
Your Name <you@example.com>
2019-05-13 20:52:17 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,61 @@
+def sort_array_asc(array)
+  return array.sort
+end
 
+def sort_array_desc(array)
+
+  return array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  return array.sort_by {|word| word.length}
+
+end
+
+def swap_elements(array)
+
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  return array.reverse!
+end
+
+#review
+def kesha_maker(array)
+  array.collect do |element|
+      element[2] = "$"
+      element
+    end
+
+
+end
+
+
+def find_a(array)
+  a_array = []
+  array.each do |word|
+    if word.start_with?("a")
+      a_array.push(word)
+    end
+  end
+  return a_array
+end
+
+#review
+def sum_array(array)
+  array.inject(0){|sum,x| sum + x }
+end
+
+#review
+def add_s(array)
+  array.collect.with_index do |word, index|
+    if index == 1
+      word
+    else
+      word + "s"
+    end
+  end
+
+end
